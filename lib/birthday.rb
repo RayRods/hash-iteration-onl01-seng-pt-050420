5e93c1d75b51c57b8d2c796c105e200cd07a7722
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-18 01:18:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,10 @@
 
 def happy_birthday(birthday_kids)
       birthday_kids.each do |kids_name, age|
-        if age >= 12
-        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+        if age < 12
+           puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+        elsif
+           puts "You're not a kid!"
         end
     end
 end
